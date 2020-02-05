0bc2c1688d145f09590d9bc31c1c4540b840a926
bernat <elbernat@gmail.com>
2020-02-05 09:23:48 -0500

    update

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 // JS
 console.log('connected');
 
-let x = document.querySelector('.x');
-// console.log(x);
-let y = document.querySelector('.y');
-// console.log(y);
-let z = document.querySelector('.z');
-// console.log(z);
+let PARAMS = {
+  alpha : 0,
+  beta : 0,
+  gamma : 0
+}
 
 if(window.DeviceOrientationEvent) {
 
   console.log("Device Orientation is supported");
 
   window.addEventListener('deviceorientation', (eventData) => {
-    x.innerHTML = `x: ${eventData.alpha } `;
-    y.innerHTML = `y: ${eventData.beta  } `;
-    z.innerHTML = `z: ${eventData.gamma } `;
+    PARAMS.alpha  = eventData.alpha;
+    PARAMS.beta   = eventData.beta;
+    PARAMS.gamma  = eventData.gamma;
   }, false);
 } else {
   console.log("DeviceOrientaion is NOT supported")
 }
 
+//////////////// TWEAKPANE ////////////////
+// TWEAKPANE - MONITOR - ACCELEROMETER
+const paneAcc = new Tweakpane({
+	container: document.getElementById('tweakpane-1')
+});
+// paneAcc.addInput(PARAMS, 'acc',{ label: 'ACCEL (X,Y)'});
+paneAcc.addMonitor(PARAMS, 'alpha', { label: 'X ACCELERATION' });
+paneAcc.addMonitor(PARAMS, 'beta', { label: 'Y ACCELERATION' });
+paneAcc.addMonitor(PARAMS, 'gamma', { label: 'Z ACCELERATION' });
+
+//////////////// TWEAKPANE ////////////////
