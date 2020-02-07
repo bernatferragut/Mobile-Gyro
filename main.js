9c40091b5a3684082b0c4ff77ea72b62754d8f81
bernat <elbernat@gmail.com>
2020-02-06 20:33:30 -0500

    update

--- main.js
+++ main.js
@@ -1,35 +1,74 @@
 // JS
 console.log('connected');
 
+//////////////// FIREBASE/////////////
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBEjTnvCCkM4bRr73PJrCbC3HQ46p6cK5I",
+    authDomain: "lzrbit-db.firebaseapp.com",
+    databaseURL: "https://lzrbit-db.firebaseio.com",
+    projectId: "lzrbit-db",
+    storageBucket: "lzrbit-db.appspot.com",
+    messagingSenderId: "488933348339",
+    appId: "1:488933348339:web:eb448d3c1c2d26b3973886",
+    measurementId: "G-TBC5QYT0H4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  let firestore = firebase.firestore();
+
+//////////////// DATA ////////////////
 let PARAMS = {
   x : 0,
   y : 0,
 }
+//////////////// DATA ////////////////
 
-
-// This data will work when the mobile is used horizontally.
+//////////////// DEVICE ORIENTATION WINDOW EVENT ////////////////
 if(window.DeviceOrientationEvent) {
 
   console.log("Device Orientation is supported");
 
   window.addEventListener('deviceorientation', (eventData) => {
-    // PARAMS.alpha  = eventData.alpha;
+    // LEFT - RIGHT ORIENTATION
     PARAMS.x   = eventData.beta; // In degree in the range [-180,180]
-    if (x >90) { x = 90 };
+    if (x >90) { x = 90 }; // to avoid screen flipping
     if (x < -90) { x = -90};
+
+    // NORTH - SOUTH ORIENTATION
     PARAMS.y  = eventData.gamma; // In degree in the range [-90,90]
+
+    // SEND PARAMS TO DB - FIND OUT
+
   }, false);
 } else {
-  console.log("DeviceOrientaion is NOT supported")
+  window.alert("DeviceOrientaion is NOT supported");
 }
+//////////////// DEVICE ORIENTATION WINDOW EVENT ////////////////
 
-//////////////// TWEAKPANE ////////////////
+//////////////// TWEAKPANE ////////////////////////////////
 // TWEAKPANE - MONITOR - ACCELEROMETER
 const paneAcc = new Tweakpane({
 	container: document.getElementById('tweakpane-1')
 });
-// paneAcc.addMonitor(PARAMS, 'alpha', { label: 'ALPHA' });
 paneAcc.addMonitor(PARAMS, 'x', { label: 'W <-> E |  X: ' });
 paneAcc.addMonitor(PARAMS, 'y', { label: 'N <-> S | Y: ' });
+// TWEAKPANE - BUTTONS
 
-//////////////// TWEAKPANE ////////////////
+
+//////////////// TWEAKPANE ////////////////////////////////
+
+
+/////////////// FIREBASE /////////////////////////////////
+  // Creating a db Reference
+  let dbRef = firestore.doc('gyroApp/data');
+  // sending data to firebase
+  dbRef.set({
+    x : PARAMS.x,
+    y : PARAMS.y,
+  }).then(() => {
+    console.log('data added')
+  }).catch((err) => {
+    console.log('got an error', err);
+  });
+  /////////////// FIREBASE /////////////////////////////////
